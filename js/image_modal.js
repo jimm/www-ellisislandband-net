27c32720e609900d57b22cc614f5ab8d7a0caca8
Jim Menard <jim@theinfinitereality.com>
2022-05-14 14:47:23 -0400

    update

--- js/image_modal.js
+++ js/image_modal.js
@@ -1,10 +1,15 @@
+window.onkeyup = function(event) {
+  if (event.keyCode == 27)
+    document.getElementById('myModal').style.display = 'none';
+}
+
 function modal_image(img) {
   const modal = document.getElementById('myModal');
   const modal_img = document.getElementById('img01');
   const caption = document.getElementById('caption');
 
-  modal.style.display = 'block';
   modal_img.src = img.src;
   modal_img.alt = img.alt;
   caption.innerHTML = img.alt;
+  modal.style.display = 'block';
 }
