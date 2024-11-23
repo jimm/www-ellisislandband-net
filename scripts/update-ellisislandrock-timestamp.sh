199b9814eeffd7adcc9b5ff4fd066468949fd3e3
Jim Menard <jmenard@seatgeek.com>
2024-11-23 17:04:45 -0500

    fixed timestamp script

--- scripts/update-ellisislandrock-timestamp.sh
+++ scripts/update-ellisislandrock-timestamp.sh
@@ -6,10 +6,20 @@ output_dir="${1:-$HOME/apps/ellis-island}"
 
 cd "$output_dir"
 ts="$(date "+%s")"
+
+# Remember old timestamped links
 old_ts_files="$(ls js/all-*.js)"
-ln -s js/all.js js/all-${ts}.js
+
+# Create new timestamped link
+cd js
+ln -s all.js all-${ts}.js
+cd ..
+
+# Replace "all.js" with timestamped name in all HTML files
 find . -name '*.html' -print0 \
     | xargs -0 sed -i -E "s#js/all(-[0-9]+)?\\.js#js/all-${ts}.js#"
+
+# Remove all but the most recent three old timestamped links
 if [ -n "$old_ts_files" ] ; then
     rm $old_ts_files            # do not quote
 fi
